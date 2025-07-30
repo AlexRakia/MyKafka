e67ae83da2395c93ed22755e375cedd8c9288eac
AlexRakia <AlexRakia@Gmail.com>
2025-07-30 14:08:19 +0300

    each consumer has a specific dynamicGroup name

--- Simple-Kafka/Simple-Kafka-Consumer/Program.cs
+++ Simple-Kafka/Simple-Kafka-Consumer/Program.cs
@@ -2,15 +2,16 @@
 
 namespace Simple_Kafka_Consumer
 {
-	internal class Program
-	{
-		static string server = "127.0.0.1:9092";
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Consuming...");
-			Console.Write("Provide a topic to consume: ");
-			var topic = Console.ReadLine();
-			Simple_Kafka_Consumer.ConsumeMessages(server, topic);
-		}
-	}
+    internal class Program
+    {
+        static string server = "127.0.0.1:9092";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consuming...");
+            Console.Write("Provide a topic to consume: ");
+            var topic = Console.ReadLine();
+            string dynamicGroup = $"consumer-group-{DateTime.Now:yyyyMMdd-HHmmss-fff}";
+            Simple_Kafka_Consumer.ConsumeMessages(server, topic, dynamicGroup);
+        }
+    }
 }
